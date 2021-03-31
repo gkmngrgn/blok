b9d5f25b0194386f80720e533674067e8711be87
Gökmen Görgen <gkmngrgn@gmail.com>
2021-03-31 16:54:15 +0200

    not necessary to use requests for testing.

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,35 +1,14 @@
-import functools
-
 import flask
-import pytest
-import requests
 
 from blok.http_server import app
 
 app.config["SERVER_NAME"] = "0.0.0.0:8080"
+app.config["TESTING"] = True
 
 
-def send_get_request(endpoint: str) -> requests.Response:
+def send_get_request(endpoint: str) -> flask.Response:
     with app.app_context():
         url = flask.url_for(endpoint)
-    return requests.get(url)
-
-
-def server_required(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        is_server_up = True
-
-        try:
-            response = send_get_request("ping")
-        except requests.exceptions.ConnectionError:
-            is_server_up = False
-        else:
-            is_server_up = response.status_code == 200
-
-        if is_server_up is False:
-            return pytest.skip("http server is not active. skipping...")
-
-        return func(*args, **kwargs)
-
-    return wrapper
+    with app.test_client() as client:
+        response = client.get(url)
+    return response
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,13 +1,11 @@
-from .test_helper import send_get_request, server_required
+from .test_helper import send_get_request
 
 
-@server_required
 def test_first_chain_data():
     response = send_get_request("chain")
     assert response.status_code == 200
 
-    response_data = response.json()
-    chain = response_data.get("chain", [])
+    chain = response.json.get("chain", [])
     assert len(chain) >= 1  # at least there should be a block in the chain.
 
     block = chain[0]
@@ -17,16 +15,11 @@ def test_first_chain_data():
     assert block["transactions"] == []
 
 
-@server_required
 def test_mining():
     response = send_get_request("mine")
     assert response.status_code == 200
 
-    response_data = response.json()
-    previous_hash = response_data["block_data"]["block_hash"]
-
-    response = send_get_request("/mine/")
+    previous_hash = response.json["block_data"]["block_hash"]
+    response = send_get_request("mine")
     assert response.status_code == 200
-
-    response_data = response.json()
-    assert previous_hash == response_data["block_data"]["previous_hash"]
+    assert previous_hash == response.json["block_data"]["previous_hash"]
